f965f5e952f415e3fd24d2a1f6b2fca47cfba505
SANTIAGO0212 <santiagocj0212@gmail.com>
2025-10-14 18:21:24 -0500

    Using an array, the menu items are created statically for the landing page

---
+++ src/data/menu.js
@@ -0,0 +1,42 @@
+export const menuItems = [
+    {
+        id:1,
+        title: 'The Ultimate Burguer',
+        Subtitle: 'Doble Carne, cheddar fundido',
+        ingredients: ['Pan artesanal', 'Doble carne 200g', 'Chedddar', 'Lechuga', 'Tomate', 'Cebolla Morada', 'Salsa Secreta de la casa'],
+        price: 24000,
+        image: new URL('../assets/burguers/burguer-1.png', import.meta.url)
+    },
+    {
+        id:2,
+        title: 'Spicy Crunch Burguer',
+        Subtitle: 'Hamburguesa Picante y Crocante',
+        ingredients: ['Pan brioche', 'Doble con especias', 'Onion rings', 'Lechuga', 'Salsa chipotle', 'Queso'],
+        price: 22000,
+        image: new URL('../assets/burguers/burguer-2.png', import.meta.url)
+    },
+    {
+        id:3,
+        title: 'Green Garden Veggie',
+        Subtitle: 'Veggie con un delicioso sabor',
+        ingredients: ['Pan integral', 'Hamburguesa vegetal', 'Aguactate', 'Mix hojas', 'Tomate', 'Alioli'],
+        price: 20000,
+        image: new URL('../assets/burguers/burguer-3.png', import.meta.url)
+    },
+    {
+        id:4,
+        title: 'Cheese Madness',
+        Subtitle: 'La hamburguesa preferida de todos los amantes al !!!QUESOOO¡¡¡',
+        ingredients: ['Pan con semillas', 'Carne', 'Chedddar', 'Triple queso', 'Pepinillos', 'Cebolla caramelizada'],
+        price: 26000,
+        image: new URL('../assets/burguers/burguer-4.png', import.meta.url)
+    },
+    {
+        id:5,
+        title: 'Crispy Fries',
+        Subtitle: 'Deliciosas papas crocantes',
+        ingredients: ['Papas rústicas', 'Salsa ketchup', 'Queso rallado'],
+        price: 8000,
+        image: new URL('../assets/burguers/fries.png', import.meta.url)
+    },
+]
